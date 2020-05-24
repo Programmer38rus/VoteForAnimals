35d72f73e5d182592a69e1f95dad7ae971d649fb
im83 <programmer38rus@gmail.com>
2020-05-24 18:40:39 +0800

    backend

--- backend/app/db.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy as sa
-
-metadata = sa.MetaData()
-
-vote_results = sa.Table('voting', metadata,
-    sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('name', sa.Text),
-    sa.Column('votes', sa.Integer)
-)
--- backend/main.py
+++
@@ -1,8 +0,0 @@
-import bottle
-
-from app.server import app
-
-if __name__ == "__main__":
-    bottle.run(
-        app=app, host=app.config.host, port=app.config.port, server=app.config.server
-    )
